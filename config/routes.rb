199e3ab51668190701a4a7ad6121aee21a31f1f4
jbarbuto <nygfan992@gmail.com>
2020-03-30 13:13:57 -0400

    Added feedback to nav bar and made alternate home page

--- config/routes.rb
+++ config/routes.rb
@@ -96,6 +96,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/metrics'
 
+  get 'static_pages/home2'
+
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
