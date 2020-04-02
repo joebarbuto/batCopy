fb40c6294e24defd195a6f2eb69c240e1b6ab716
Andrea Fischetti <aefischetti@gmail.com>
2020-04-02 15:54:32 -0400

    newpage

--- config/routes.rb
+++ config/routes.rb
@@ -103,5 +103,7 @@ Rails.application.routes.draw do
   get 'static_pages/home2'
 
   root 'static_pages#home'
+
+  get 'static_pages/home3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
